9833e1b706309903777acee1a78f6d5a7c8d10c1
Juan Perata <juan@bespoken.tools>
2018-11-23 11:19:09 -0500

    Go back to objects for string and number

--- dist/js-yaml.js
+++ dist/js-yaml.js
@@ -1,4 +1,4 @@
--n /* js-yaml-bespoken 3.11.3 https://github.com/nodeca/js-yaml-bespoken */
+-n /* js-yaml-bespoken 3.11.4 https://github.com/nodeca/js-yaml-bespoken */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.jsyaml = f()}})(function(){var define,module,exports;return (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 'use strict';
 
@@ -1111,9 +1111,9 @@ function addMetaInformation(state, line) {
     if (state.result === 'null') {
       return;
     }
-    state.result = String(state.result);
+    state.result = new String(state.result);
   } else if (typeof state.result === 'number') {
-    state.result = Number(state.result);
+    state.result = new Number(state.result);
   }
 
   if (!line) {
